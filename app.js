b8fb61db8e817e22740032a9df79cdf0ee1e7ef6
Daria <mar.031281@gmail.com>
2022-10-18 11:26:15 +0300

    add couners items

--- app.js
+++ app.js
@@ -47,12 +47,54 @@ $(".slide").slick({
 function scrollFunction() {
     if (window.pageYOffSet > document.body.scrollHeight / 2 ) { 
         showModal();
-        window.removeEventListener("scroll", scrollFunction)
+        window.removeEventListener("scroll", scrollFunction);
     }
 }
 window.addEventListener("scroll", scrollFunction);
 //animation 
 AOS.init();
+//change product quantity
+// let audi = {
+//     year: "2020",
+//     color: "blue",
+//     model: "A4",
+// }
+// функція конструктор
+let incrementButh = document.querySelectorAll(".increment");
+let decrementButh = document.querySelectorAll(".decrement");
+let productQuantity = document.querySelectorAll(".product-quantity input");
+function Counter(incrementButton,
+    decrementButton,
+    inputField, 
+    minCount = 1, 
+    maxCount = 5) {
+    this.domRefs = {
+        incrementButton,
+        decrementButton,
+        inputField,
+    };
+    this.toggleButtonState = function() {
+        let count = this.domRefs.inputField.value;
+        this.domRefs.decrementButton.disabled = count <= minCount;
+        this.domRefs.incrementButton.disabled = count >= maxCount;
+    }
+    this.toggleButtonState();
+    this.increment = function() {
+        this.domRefs.inputField.value = +this.domRefs.inputField.value + 1;
+        this.toggleButtonState();
+    }
+    this.decrement = function() {
+        this.domRefs.inputField.value = +this.domRefs.inputField.value - 1;
+        this.toggleButtonState();
+    }
+    this.domRefs.incrementButton.addEventListener("click", this.increment.bind(this));
+    this.domRefs.decrementButton.addEventListener("click", this.decrement.bind(this));
+}
+let counters = [];
+productQuantity.forEach(
+    (item,i) =>
+    (counters[i] = new Counter(incrementButh[i], decrementButh[i], item))
+);
 
   
  
